2ab39997a807860993b4fafedec9aa66a5bb0e11
Tilman Beck <beck@ukp.informatik.tu-darmstadt.de>
2020-08-04 17:22:19 +0200

    - example for our data added

---
+++ CTM.py
@@ -0,0 +1,44 @@
+from contextualized_topic_models.models.ctm import CTM
+from contextualized_topic_models.utils.data_preparation import TextHandler
+from contextualized_topic_models.utils.data_preparation import bert_embeddings_from_list
+from contextualized_topic_models.datasets.dataset import CTMDataset
+import pandas as pd
+import numpy as np
+import argparse
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input")
+args = parser.parse_args()
+
+#df = pd.read_csv(args.input, sep="\t")
+#df = pd.read_csv("/home/beck/Data/temporal-argmin/trec_microblog/trec_2011_2012_2013_2014_cat2.tsv", sep="\t")
+df = pd.read_csv("/home/beck/Repositories/Data/trec_microblog/trec_2011_2012_2013_2014_cat2.tsv", sep="\t")
+
+true_labels = list(df['topicId'].values)
+texts = list(df['full_text'].values)
+ids = list(df['tweetId'].values)
+nr_topics = len(df['topicId'].unique())
+
+handler = TextHandler(texts)
+handler.prepare() # create vocabulary and training data
+
+# load BERT data
+training_bert = bert_embeddings_from_list(texts, 'bert-base-nli-mean-tokens')
+
+training_dataset = CTMDataset(handler.bow, training_bert, handler.idx2token)
+
+ctm = CTM(input_size=len(handler.vocab), bert_input_size=768, num_epochs=5, inference_type="combined",
+          n_components=nr_topics, num_data_loader_workers=5)
+
+ctm.fit(training_dataset) # run the model
+
+distribution = ctm.get_thetas(training_dataset)
+
+best_match_topics = np.argmax(distribution, axis=1)
+
+with open('predictions_CTM.txt', 'w') as  fp:
+	for ID, topicId in zip(ids, best_match_topics):
+		fp.write(ID + ' ' + topicId + '\n')
+
